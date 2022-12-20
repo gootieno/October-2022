347900f1bb62123ee16724fa9af0caf5de19a741
Tanner Shaw <traumaaa@gmail.com>
2022-12-20 13:01:02 -0600

    Create and remove elements solutions

--- Week9/Day2/03-create-remove-elements/create-remove.js
+++ Week9/Day2/03-create-remove-elements/create-remove.js
@@ -9,12 +9,27 @@ add.addEventListener("click", async () => {
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
         // Your code here
+        const urlParts = url.split('/')
+        const breed = urlParts[4];
 
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
         // Your code here
+            const newLi = document.createElement('li');
+            const newFig = document.createElement('figure');
+            const newImg = document.createElement('img');
+            const newCaption = document.createElement('figcaption');
 
+            newImg.src = url;
+            newCaption.innerText = breed;
+
+            newFig.append(newImg, newCaption)
+            newLi.appendChild(newFig)
+
+            const gallery = document.querySelector('.gallery ul');
+
+            gallery.appendChild(newLi);
         /* Add the new dog card as a child to the ul in the .gallery element */
         // Your code here
 
@@ -28,6 +43,13 @@ const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
     /*-------------------- Select the first dog card --------------------- */
     // Your code here
+    const removeDog = document.querySelector('li');
+
+    if (removeDog) {
+        removeDog.remove();
+    } else {
+        console.log('No dog to remove');
+    }
 
     /*-------------------- Remove the first dog card --------------------- */
     // Your code here
@@ -38,6 +60,16 @@ const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
     /*-------------------- Select the last dog card ----------------------- */
     // Your code here
+    const allDogs = document.querySelectorAll('li');
+
+
+    const lastDog = allDogs[allDogs.length - 1];
+    if (lastDog) {
+        lastDog.remove()
+    } else {
+        console.log('No dog to remove');
+    }
+
 
     /*-------------------- Remove the last dog card ----------------------- */
     // Your code here
