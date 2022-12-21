1786d7ae2a9fb945ca0ebf9242fc381f932b69d2
Tanner Shaw <traumaaa@gmail.com>
2022-12-21 13:04:33 -0600

    data on elements practice solutions

--- Week9/Day3/02-data-on-elements/data.js
+++ Week9/Day3/02-data-on-elements/data.js
@@ -1,1 +1,22 @@
 // Your code here
+window.addEventListener('DOMContentLoaded', () => {
+
+  const shoppingList = document.getElementById('shopping-list')
+  const button = document.getElementById('add');
+
+  button.addEventListener('click', e => {
+    e.preventDefault();
+    const newLi = document.createElement('li');
+    const input = document.getElementById('name');
+    const dataType = document.getElementById('type');
+    const inputValue = input.value;
+    console.log(input);
+    const data = dataType.value;
+
+    newLi.innerText = inputValue;
+    newLi.setAttribute('data-type', data);
+
+    shoppingList.append(newLi);
+  })
+
+})
