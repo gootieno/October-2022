0063c890af720d9dc07c2e980da0de4da396ff3e
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-12-22 09:41:51 -0800

    web storage solution

--- Week9/Day4/solutions/01-web-storage/public/main.js
+++ Week9/Day4/solutions/01-web-storage/public/main.js
@@ -3,17 +3,23 @@
 // For storing user's theme selection in the browser
 function storeTheme(themeName) {
   // Your code here
+  localStorage.setItem("themeName", themeName);
 }
 
 // For restoring theme, if selected by the user in the past
 function restoreTheme() {
   // Your code here
+  const themeName = localStorage.getItem("themeName");
+  if (themeName) setTheme(themeName);
 }
 
 // For clearing theme selection from the browser's storage (reset to default)
 
 function clearTheme() {
   // Your code here
+
+  //   localStorage.clear();
+  localStorage.removeItem("themeName");
 }
 
 /* ================================ PHASE 2 ================================ */
@@ -21,16 +27,21 @@ function clearTheme() {
 // For storing user's display name
 function storeName(displayName) {
   // Your code here
+  sessionStorage.setItem("display", displayName);
 }
 
 // For restoring user's display name, if set in the past
 function restoreName() {
   // Your code here
+  const name = sessionStorage.getItem("display");
+
+  if (name) setInputValue("display-name", name);
 }
 
 // For clearing user's display name from browser storage
 function clearName() {
   // Your code here
+  sessionStorage.removeItem("display");
 }
 
 /* ========================================================================= */
