07bbc05def1e5c715d31d312eb231121eb0cf556
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-12-22 11:14:52 -0800

    all storge solutions

--- Week9/Day4/03-catsagram-long-practice-day-3/public/main.js
+++ Week9/Day4/03-catsagram-long-practice-day-3/public/main.js
@@ -1,52 +1,82 @@
-import { resetScore } from './score.js';
-import { resetComments } from './comments.js';
+import { resetScore } from "./score.js";
+import { resetComments } from "./comments.js";
 
 export const createMainContent = () => {
-    // Create h1
-    const h1 = document.createElement("h1");
-    h1.innerText = "Catstagram";
+  // Create h1
+  const h1 = document.createElement("h1");
+  h1.innerText = "Catstagram";
 
-    // Create img
-    const img = document.createElement("img");
-    img.style.margin = "20px";
-    img.style.maxWidth = "750px";
+  // Create img
+  const img = document.createElement("img");
+  img.style.margin = "20px";
+  img.style.maxWidth = "750px";
 
-    const newKittenBtn = createNewKittenBtn();
+  const newKittenBtn = createNewKittenBtn();
 
-    const container = document.querySelector(".container");
-    container.appendChild(h1);
-    container.append(newKittenBtn);
-    container.appendChild(img);
+  const container = document.querySelector(".container");
+  container.appendChild(h1);
+  container.append(newKittenBtn);
+  container.appendChild(img);
 
-    fetchImage();
+  fetchImage();
 };
 
 const fetchImage = async () => {
-    // Fetch image from API and set img url
-    try {
-        const kittenResponse = await fetch("https://api.thecatapi.com/v1/images/search?size=small");
-        // Converts to JSON
-        const kittenData = await kittenResponse.json();
-        // console.log(kittenData);
-        const kittenImgUrl = kittenData[0].url;
-        const kittenImg = document.querySelector("img");
-        kittenImg.src = kittenImgUrl;
+  // Fetch image from API and set img url
+  let kittenImgUrl = localStorage.getItem("cat-image");
+  console.log("cat image", kittenImgUrl);
 
-        // After the image is finished loading, reset the score and comments
-        kittenImg.addEventListener('load', () => {
-            resetScore();
-            resetComments();
-        });
+  if (kittenImgUrl) {
+    const kittenImg = document.querySelector("img");
+    kittenImg.src = kittenImgUrl;
+  } else {
+    try {
+      const kittenResponse = await fetch(
+        "https://api.thecatapi.com/v1/images/search?size=small"
+      );
+      // Converts to JSON
+      const kittenData = await kittenResponse.json();
+      // console.log(kittenData);
+      kittenImgUrl = kittenData[0].url;
+      localStorage.setItem("cat-image", kittenImgUrl);
+      const kittenImg = document.querySelector("img");
+      kittenImg.src = kittenImgUrl;
+
+      // After the image is finished loading, reset the score and comments
+      kittenImg.addEventListener("load", () => {
+        resetScore();
+        resetComments();
+      });
     } catch (e) {
-        console.log("Failed to fetch image", e);
+      console.log("Failed to fetch image", e);
     }
+  }
+};
+
+const getNewCat = async () => {
+  const kittenResponse = await fetch(
+    "https://api.thecatapi.com/v1/images/search?size=small"
+  );
+  // Converts to JSON
+  const kittenData = await kittenResponse.json();
+  // console.log(kittenData);
+  const kittenImgUrl = kittenData[0].url;
+  localStorage.setItem("cat-image", kittenImgUrl);
+  const kittenImg = document.querySelector("img");
+  kittenImg.src = kittenImgUrl;
+
+  // After the image is finished loading, reset the score and comments
+  kittenImg.addEventListener("load", () => {
+    resetScore();
+    resetComments();
+  });
 };
 
 const createNewKittenBtn = () => {
-    // Create "New Kitten" button
-    const newKittenBtn = document.createElement("button");
-    newKittenBtn.id = "new-kitten";
-    newKittenBtn.innerText = "New Kitten";
-    newKittenBtn.addEventListener('click', fetchImage);
-    return newKittenBtn;
+  // Create "New Kitten" button
+  const newKittenBtn = document.createElement("button");
+  newKittenBtn.id = "new-kitten";
+  newKittenBtn.innerText = "New Kitten";
+  newKittenBtn.addEventListener("click", getNewCat);
+  return newKittenBtn;
 };
--- Week9/Day4/solutions/02-cookies/public/main.js
+++ Week9/Day4/solutions/02-cookies/public/main.js
@@ -2,34 +2,84 @@
 
 // For storing user's theme selection in cookies
 function storeTheme(themeName) {
-    // Your code here
+  // Your code here
+  document.cookie = `themeName=${themeName}; max-age=15`;
 }
 
 // For restoring theme from cookies, if selected by the user in the past
 function restoreTheme() {
-    // Your code here
+  // Your code here
+
+  //   const documentCookies = document.cookie;
+  //   console.log("document cookies ", documentCookies);
+
+  //   const themeName = documentCookies.split("=")[1];
+
+  //   console.log("themeName ", themeName);
+
+  //   if (themeName) setTheme(themeName);
+
+  // multiple cookies
+
+  let cookies = document.cookie;
+  console.log("cookie ", cookies);
+  let cookiesArr = cookies.split("; ");
+
+  let themeCookie;
+  let theme;
+
+  for (let cookie of cookiesArr) {
+    if (cookie.startsWith("themeName")) {
+      themeCookie = cookie;
+      theme = themeCookie.split("=")[1];
+      console.log("theme ", theme);
+    }
+  }
+
+  if (theme) {
+    setTheme(theme);
+  }
 }
 
 // For clearing theme selection from cookies (reset to default)
 function clearTheme() {
-    // Your code here
+  // Your code here
+  document.cookie = `themeName=;max-age=0`;
 }
 
 /* ================================ PHASE 3 ================================ */
 
 // For storing user's display name in cookies
 function storeName(displayName) {
-    // Your code here
+  // Your code here
+  document.cookie = `display=${displayName}`;
 }
 
 // For restoring user's display name from cookies, if set in the past
 function restoreName() {
-    // Your code here
+  // Your code here
+  let cookies = document.cookie;
+  let cookiesArr = cookies.split(";");
+
+  let displayCookie;
+  let display;
+
+  for (let cookie of cookiesArr) {
+    if (cookie.startsWith("display")) {
+      displayCookie = cookie;
+      display = displayCookie.split("=")[1];
+    }
+  }
+
+  if (display) {
+    setInputValue("display-name", display);
+  }
 }
 
 // For clearing user's display name from cookies
 function clearName() {
-    // Your code here
+  // Your code here
+  document.cookie = `display=;max-age=0`;
 }
 
 /* ========================================================================= */
@@ -41,52 +91,52 @@ function clearName() {
 // For changing one theme button's styling to indicate which theme is selected
 
 function toggleButtonSelection(themeName, selected) {
-    const btn = document.getElementById(`theme-button-${themeName}`);
-    if (btn) {
-        if (selected) {
-            btn.classList.add('selected');
-        } else {
-            btn.classList.remove('selected');
-        }
+  const btn = document.getElementById(`theme-button-${themeName}`);
+  if (btn) {
+    if (selected) {
+      btn.classList.add("selected");
+    } else {
+      btn.classList.remove("selected");
     }
+  }
 }
 
 // Use a particular theme
 
 function setTheme(themeName) {
-    // Clear previous selection so buttons don't get stuck in selected state
-    resetTheme()
+  // Clear previous selection so buttons don't get stuck in selected state
+  resetTheme();
 
-    // Remember user's selection by storing it in their browser
-    storeTheme(themeName);
+  // Remember user's selection by storing it in their browser
+  storeTheme(themeName);
 
-    // Apply the theme to the page document
-    document.documentElement.className = `theme-${themeName}`;
+  // Apply the theme to the page document
+  document.documentElement.className = `theme-${themeName}`;
 
-    // Show which button is selected
-    toggleButtonSelection(themeName, true);
+  // Show which button is selected
+  toggleButtonSelection(themeName, true);
 }
 
 // Use default theme
 
 function resetTheme() {
-    // Remove selection styling from all buttons
-    toggleButtonSelection('dragon', false);
-    toggleButtonSelection('griffin', false);
-    toggleButtonSelection('wizard', false);
+  // Remove selection styling from all buttons
+  toggleButtonSelection("dragon", false);
+  toggleButtonSelection("griffin", false);
+  toggleButtonSelection("wizard", false);
 
-    // Set default theme so header and footer are contrast colors
-    document.documentElement.className = `theme-none`;
+  // Set default theme so header and footer are contrast colors
+  document.documentElement.className = `theme-none`;
 }
 
 // For adding event listeners on the theme buttons
 
 function addThemeEventListeners() {
-    const themeNames = ['dragon', 'griffin', 'wizard'];
-    themeNames.forEach(themeName => {
-        const button = document.getElementById(`theme-button-${themeName}`);
-        button.addEventListener('click', () => setTheme(themeName));
-    });
+  const themeNames = ["dragon", "griffin", "wizard"];
+  themeNames.forEach((themeName) => {
+    const button = document.getElementById(`theme-button-${themeName}`);
+    button.addEventListener("click", () => setTheme(themeName));
+  });
 }
 
 // ===== NAME CONTROL
@@ -94,27 +144,27 @@ function addThemeEventListeners() {
 // For assigning change event to input field
 
 function assignChangeEvent(inputId, handleChange) {
-    const input = document.getElementById(inputId);
-    if (input) {
-        input.addEventListener('input', (event) => {
-            handleChange(event.target.value);
-        });
-    }
+  const input = document.getElementById(inputId);
+  if (input) {
+    input.addEventListener("input", (event) => {
+      handleChange(event.target.value);
+    });
+  }
 }
 
 // For setting value on input field
 
 function setInputValue(inputId, value) {
-    const input = document.getElementById(inputId);
-    if (input) {
-        input.value = value;
-    }
+  const input = document.getElementById(inputId);
+  if (input) {
+    input.value = value;
+  }
 }
 
 // For resetting the display name to empty string
 
 function resetName() {
-    setInputValue('display-name', '');
+  setInputValue("display-name", "");
 }
 
 // ===== CLEAR ALL
@@ -122,20 +172,20 @@ function resetName() {
 // For the clear/reset button
 
 function clearAll() {
-    // Remove from browser storage
-    clearTheme();
-    clearName();
+  // Remove from browser storage
+  clearTheme();
+  clearName();
 
-    // Reset the page
-    resetTheme();
-    resetName();
+  // Reset the page
+  resetTheme();
+  resetName();
 }
 
 // For adding click event listener on the Clear All button
 
 function addClearAllEventListener() {
-    const button = document.getElementById("clear-all");
-    button.addEventListener('click', clearAll);
+  const button = document.getElementById("clear-all");
+  button.addEventListener("click", clearAll);
 }
 
 // ===== INITIALIZE PAGE
@@ -143,21 +193,21 @@ function addClearAllEventListener() {
 // For setting a theme when the page loads (called by body's onload event)
 
 function initializePage() {
-    // Set default theme so header & footer have dark backgrounds
-    resetTheme();
+  // Set default theme so header & footer have dark backgrounds
+  resetTheme();
 
-    // Restore user's previous theme selection, if it exists
-    restoreTheme();
+  // Restore user's previous theme selection, if it exists
+  restoreTheme();
 
-    // Assign event to name input
-    assignChangeEvent('display-name', storeName);
+  // Assign event to name input
+  assignChangeEvent("display-name", storeName);
 
-    // Restore user's previous name selection, if it exists
-    restoreName();
+  // Restore user's previous name selection, if it exists
+  restoreName();
 
-    // Add event listeners
-    addThemeEventListeners();
-    addClearAllEventListener();
+  // Add event listeners
+  addThemeEventListeners();
+  addClearAllEventListener();
 }
 
-window.addEventListener('DOMContentLoaded', initializePage);
+window.addEventListener("DOMContentLoaded", initializePage);
